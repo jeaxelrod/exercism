9aed51c2bb4814e4b5e2e59cffd5189479a0c76e
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-11-19 17:16:28 -0800

    Metaprogramming solution for rna-transcription

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,17 +1,22 @@
 class Complement
-
-  DNA_TO_RNA = {'G' => 'C', 'C' => 'G', 'A' => 'U', 'T' => 'A'}
+  DNA_TO_RNA = {'A' => 'U', 'T' => 'A', 'C' => 'G', 'G' => 'C'}
   RNA_TO_DNA = DNA_TO_RNA.invert
+  CONVERSIONS = {'dna' => DNA_TO_RNA, 'rna' => RNA_TO_DNA}
 
-  def self.of_dna(dna)
-    dna.split("").inject("") do |rna, nucleotide|
-      rna += DNA_TO_RNA[nucleotide]
+  def self.method_missing(method, *args, &block)
+    if method.to_s =~ /^of_(.+)$/
+      nucleic_acid_type = method.to_s.match(/^of_(.+)$/)[1]
+      strand = args[0].split("").inject("") do |strand, nucleotide|
+        strand += complement(nucleic_acid_type, nucleotide)
+      end
+      return strand
     end
+    super
   end
 
-  def self.of_rna(rna)
-    rna.split("").inject("") do |dna, nucleotide|
-      dna += RNA_TO_DNA[nucleotide]
-    end
+  private
+
+  def self.complement(nucleic_acid_type, nucleotide)
+    CONVERSIONS[nucleic_acid_type][nucleotide]
   end
 end
