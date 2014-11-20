aa6c2de31cecc67e1f515e102b86d5b7b815a1a6
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-11-19 16:55:18 -0800

    Add two solutions

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,10 @@
 class Hamming
   def self.compute(strand1, strand2)
-    return 0 if strand1 == strand2
-    strand_arr1 = strand1.split("")
-    strand_arr2 = strand2.split("")
     differences = 0
-    strand_arr1.each_index do |index|
-      nucleotide1 = strand_arr1[index]
-      nucleotide2 = strand_arr2[index]
-      return differences unless nucleotide1 && nucleotide2
+    limit = [strand1.length, strand2.length].min
+    (0...limit).each do |index|
+      nucleotide1 = strand1[index]
+      nucleotide2 = strand2[index]
       differences += 1 if nucleotide2 != nucleotide1
     end
     differences
