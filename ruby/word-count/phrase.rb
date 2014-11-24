df0c603302590c2a9b8c01630edd1bf77dbe34b4
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-11-24 12:56:41 -0800

    Refactor phrase

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,11 +1,11 @@
 class Phrase
-  attr_reader :words, :count
+  attr_reader :words, :word_count
   def initialize words
     @words = words.downcase.split(/[^\w']+/)
   end
 
   def word_count
-    @count ||= @words.inject(Hash.new(0)) do |hash, word| 
+    @word_count ||= @words.inject(Hash.new(0)) do |hash, word| 
       hash[word] += 1
       hash
     end
