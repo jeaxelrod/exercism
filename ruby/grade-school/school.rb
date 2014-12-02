23e42359932af5bc11618f8fcb1a508bdbc4e2a8
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-12-02 12:44:58 -0800

    Refactor school

--- ruby/grade-school/school.rb
+++ ruby/grade-school/school.rb
@@ -1,18 +1,17 @@
 class School
   def initialize
-    @students = {} 
+    @students = Hash.new [] 
   end
   
   def to_hash
     sorted_students = {}
     @students.keys.sort.each { |key| sorted_students[key] = @students[key] }
-    sorted_students
+    @students = sorted_students
   end
 
   def add name, grade
-    @students[grade] ? @students[grade].push(name) : @students[grade] = [name]
+    @students[grade] += [name]
     @students[grade].sort!
-    @students.keys.sort!
   end
 
   def grade grade
